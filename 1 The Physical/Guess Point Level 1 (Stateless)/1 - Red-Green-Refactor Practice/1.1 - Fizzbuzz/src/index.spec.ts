424839079ef7b64b12aff53978113f7b2a959e92
Santosh <you@example.com>
2023-03-17 09:35:30 -0500

    improved test case naming to clearly state the expected behavior, using one describe block with multiple tests in it

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
@@ -3,38 +3,27 @@ import { fizzBuzz } from "./fizzbuzz";
  
 
 describe('fizzBuzz module', () => {
-  test('test input number 0', () => {
+  test('test input number 0 returns Enter numbers between 1 to 100', () => {
     expect(fizzBuzz(0)).toBe('Enter numbers between 1 to 100');
   });
-}); 
 
-describe('fizzBuzz module', () => {
-  test('test input number 101', () => {
+  test('test input number 101 returns Enter numbers between 1 to 100', () => {
     expect(fizzBuzz(101)).toBe('Enter numbers between 1 to 100');
   });
-}); 
 
-describe('fizzBuzz module', () => {
-  test('test input number 3', () => {
+  test('test input number 3 returns Fizz', () => {
     expect(fizzBuzz(3)).toBe('Fizz');
   });
-}); 
 
-describe('fizzBuzz module', () => {
-  test('test input number 15', () => {
+  test('test input number 15 returns FizzBuzz', () => {
     expect(fizzBuzz(15)).toBe('FizzBuzz');
   });
-}); 
 
-describe('fizzBuzz module', () => {
-  test('test input number 10', () => {
+  test('test input number 10 returns Buzz', () => {
     expect(fizzBuzz(10)).toBe('Buzz');
   });
-}); 
 
-
-describe('fizzBuzz module', () => {
-  test('test input number 8', () => {
+  test('test input number 8 returns 8', () => {
     expect(fizzBuzz(8)).toBe('8');
   });
 }); 
